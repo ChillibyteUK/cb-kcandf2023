e4b41d652e255d0a4fe50229603a1d11f031a65b
CheechRockwizard <github@lamcat.co.uk>
2023-08-25 11:58:27 +0100

    agency case studies

--- inc/cb-taxonomies.php
+++ inc/cb-taxonomies.php
@@ -47,27 +47,6 @@ function cb_register_taxes()
     ];
     register_taxonomy("process", [ "case-studies" ], $args);
 
-    $args = [
-        "label" => __("Team", "cb-kcandf2023"),
-        "labels" => [
-            "name" => __("Teams", "cb-kcandf2023"),
-            "singular_name" => __("Team", "cb-kcandf2023"),
-        ],
-        "public" => true,
-        "publicly_queryable" => false,
-        "hierarchical" => true,
-        "show_ui" => true,
-        "show_in_menu" => true,
-        "show_in_nav_menus" => true,
-        "query_var" => true,
-        "rewrite" => false,
-        "show_admin_column" => true,
-        "show_in_rest" => true,
-        "show_tagcloud" => false,
-        "show_in_quick_edit" => true,
-        "show_in_graphql" => false,
-    ];
-    // register_taxonomy("team", [ "people" ], $args);
 
 }
 add_action('init', 'cb_register_taxes');
--- page-templates/blocks/cb_line_block.php
+++ page-templates/blocks/cb_line_block.php
@@ -1,6 +1,7 @@
 <section class="line_block">
     <div class="line_block__line" data-aos="fade"></div>
     <div class="container-xl">
-        <div class="h2 text-center" data-aos="fade"><?=get_field('content')?></div>
+        <div class="h2 text-center dot" data-aos="fade">
+            <?=get_field('content')?></div>
     </div>
 </section>
