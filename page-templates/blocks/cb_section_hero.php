6ae90b4cc2bb3ab8b41e2f1b13a0c9fdb01dc387
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-08-31 14:53:34 +0100

    section hero tweaks and font diddling

--- page-templates/blocks/cb_section_hero.php
+++ page-templates/blocks/cb_section_hero.php
@@ -7,11 +7,11 @@ $text = $orientation == 'left' ? 'text-end' : 'text-start';
 $line = $orientation == 'left' ? 'right' : 'left';
 $lineColour = $theme == 'Dark' ? '#de1b3c' : '#22806c';
 ?>
-<section class="section_hero mb-5">
+<section class="section_hero">
     <div class="container-xl">
         <div class="section_hero__images">
             <div class="section_hero__background <?=$orientation?>" data-aos="fade-<?=$orientation?>" style="background-image:url('<?=get_stylesheet_directory_uri()?>/img/bg-<?=$background?>--<?=$theme?>.svg')"></div>
-            <img src="<?=wp_get_attachment_image_src(get_field('image'),'large')?>" class="section_hero__image <?=$orientation?>" data-aos="<?=$fade?>" data-aos-offset="300">
+            <img src="<?=wp_get_attachment_image_url(get_field('image'),'large')?>" class="section_hero__image <?=$orientation?>" data-aos="<?=$fade?>" data-aos-offset="300">
         </div>
         <div class="section_hero__content sticky-top <?=$orientation?>">
             <div class="line <?=$line?>" data-aos="fade"></div>
