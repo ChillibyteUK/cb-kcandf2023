030be8fb133b53c0fe7db51c26b003aa4c98c6a8
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-09-05 16:30:33 +0100

    Update cb_cs_grid.php

--- page-templates/blocks/cb_cs_grid.php
+++ page-templates/blocks/cb_cs_grid.php
@@ -2,9 +2,10 @@
 $theme = get_field('site');
 ?>
 <div class="container-xl">
-<div class="filters my-4">
+<div class="filters my-4" id="filters">
     <a data-filter="*" class="pointer btn btn-red active mb-2">Show All</a>
     <?php
+    /*
     $terms = get_terms(array(
         'taxonomy' => 'process',
         'hide_empty' => true,
@@ -12,6 +13,7 @@ $theme = get_field('site');
 foreach ($terms as $t) {
     echo '<a data-filter=".' . $t->slug . '" class="btn btn-red mb-2">' . $t->name . '</a> ';
 }
+    */
 ?>
 </div>
 <div id="csgrid" class="row mx-0 mb-5">
@@ -23,6 +25,8 @@ $q = new WP_Query(array(
     'meta_key' => 'theme',
     'meta_value' => $theme
 ));
+$allTerms = array();
+
 while ($q->have_posts()) {
     $q->the_post();
     echo get_field('theme');
@@ -34,7 +38,7 @@ while ($q->have_posts()) {
     $cats = get_the_terms(get_the_ID(), 'process');
     $category = wp_list_pluck($cats, 'name');
     $catclass = implode(' ', array_map('cbslugify', $category));
-
+    $allTerms = array_merge($allTerms,$category);
     ?>
             <div class="<?=$catclass?> caseStudy col-md-6 col-lg-4">
                 <a class="caseStudy_card"
@@ -57,13 +61,44 @@ while ($q->have_posts()) {
 </div>
 </div>
 <?php
-add_action('wp_footer', function () {
+$uTerms = json_encode(array_values(array_unique($allTerms)));
+
+add_action('wp_footer', function () use ($uTerms) {
     ?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js"
     integrity="sha512-Zq2BOxyhvnRFXu0+WE6ojpZLOU2jdnqbrM1hmVdGzyeCa1DgM3X5Q4A/Is9xA1IkbUeDd7755dNNI/PzSf2Pew=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
     (function($) {
+
+        function slugify(str) {
+           return str.toLowerCase().replace(/\s+/g, '-');
+        }
+
+        const jsonArray = <?=$uTerms?>;
+
+        var container = document.getElementById('filters');
+
+        for (var i = 0; i < jsonArray.length; i++) {
+            var name = jsonArray[i];
+            var slug = slugify(name);
+
+            // Create <a> element
+            var aElement = document.createElement('a');
+            aElement.setAttribute('data-filter', '.'+slug);
+            aElement.className = 'btn btn-red mb-2';
+            aElement.textContent = name;
+
+            // Append the <a> element to the container
+            container.appendChild(aElement);
+
+            // Add a space for separation
+            if (i < jsonArray.length - 1) {
+                var space = document.createTextNode(' ');
+                container.appendChild(space);
+            }
+        }
+
 
         var $grid = $('#csgrid').isotope({
             itemSelector: '.caseStudy',
